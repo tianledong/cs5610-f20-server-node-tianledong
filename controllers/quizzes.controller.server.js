55d78718b2995fe1a7562dc05067a4dc4d329f11
Tianle Dong <dtllawrence@gmail.com>
2020-11-22 13:48:29 -0500

    create question and quiz controllers

---
+++ controllers/quizzes.controller.server.js
@@ -0,0 +1,15 @@
+const quizzesService
+    = require('../services/quizzes.service.server')
+module.exports = (app) => {
+
+    const findAllQuizzes = (req, res) =>
+        res.send(quizzesService.findAllQuizzes())
+    const findQuizById = (req, res) => {
+        const quizId = req.params['qid']
+        const quiz = quizzesService.findQuizById(quizId)
+        res.json(quiz)
+    }
+
+    app.get('/api/quizzes', findAllQuizzes)
+    app.get('/api/quizzes/:qid', findQuizById)
+}
