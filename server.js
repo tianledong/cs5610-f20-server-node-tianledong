9a4bff9f19e29a2b7e527791c6e888a8c7a99094
Tianle Dong <dtllawrence@gmail.com>
2020-11-25 14:56:17 -0500

    update server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
@@ -12,6 +17,7 @@ app.use(function (req, res, next) {
 
 require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
+require('./controllers/quiz-attempts.controller.server')(app)
 
 app.listen(process.env.PORT || 8080);
 
