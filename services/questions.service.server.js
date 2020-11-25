375ebb918a6cad80e698b34a8bab91b3f20ce6f2
Tianle Dong <dtllawrence@gmail.com>
2020-11-25 11:20:44 -0500

    modify question service to dao

--- services/questions.service.server.js
+++ services/questions.service.server.js
@@ -1,7 +1,5 @@
-let questions = require('./questions.json');
-findQuestionsForQuiz = (quizId) =>
-    questions.filter(question => question.quizId === quizId);
-
-module.exports = {
-    findQuestionsForQuiz
-}
+let questionsDao = require('../daos/questions.dao.server');
+const findAllQuestions = () => questionsDao.findAllQuestions()
+const findQuestionById = (qid) => questionsDao.findQuestionById(qid)
+const findQuestionsForQuiz = (qid) => questionsDao.findQuestionsForQuiz(qid)
+module.exports = {findAllQuestions, findQuestionById, findQuestionsForQuiz}
